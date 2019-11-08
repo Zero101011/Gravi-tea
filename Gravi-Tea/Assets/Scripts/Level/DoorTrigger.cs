1e14c0b5fd6d2962d1fcc1e4a9b02d9ad863c8d9
Zero101011 <geejay412@gmail.com>
2019-11-08 14:31:08 +0000

    removed legacy camera scripts (orbit camera etc) implemented simple trigger for puzzle mechanic

---
+++ Gravi-Tea/Assets/Scripts/Level/DoorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Box")
+        {
+            print("oof thats a trigger");
+        }
+    }
+}
